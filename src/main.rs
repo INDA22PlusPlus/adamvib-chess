f9c591152f038e9611982edf38812aeef12586e0
Adam Viberg <adamvib@kth.se>
2022-09-13 23:42:42 +0200

    Adding a board generator

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,56 @@
+use strum::IntoEnumIterator; // 0.17.1
+use strum_macros::EnumIter; // 0.17.1
+
 fn main() {
-    // crates an 8x8 array of zeros
-    let mut board = [[0u8; 8]; 8];
+
 }
 
+fn create_board() -> [[Piece; 8]; 8] {
+    // crates an 8x8 array of empt pieces
+    let empty_piece: Piece = Piece {
+        piece_type: PieceType::EMPTY,
+        color: Color::WHITE
+    };
+    let mut board: [[Piece; 8]; 8] = [[empty_piece; 8]; 8];
+
+    // loop through x positions and piecies
+    for (position, piece) in (0..5).zip(PieceType::iter()) {
+        board[position][0] = Piece { piece_type: piece, color: Color::WHITE };
+        board[position][1] = Piece { piece_type: PieceType::PAWN, color: Color::WHITE };
+
+        board[position][7] = Piece { piece_type: piece, color: Color::BLACK };
+        board[position][6] = Piece { piece_type: PieceType::PAWN, color: Color::BLACK };
+    }
+    for (position, piece) in (8..5).zip(PieceType::iter()) {
+        board[position][0] = Piece { piece_type: piece, color: Color::WHITE };
+        board[position][1] = Piece { piece_type: PieceType::PAWN, color: Color::WHITE };
+
+        board[position][7] = Piece { piece_type: piece, color: Color::BLACK };
+        board[position][6] = Piece { piece_type: PieceType::PAWN, color: Color::BLACK };
+    }
+
+    board
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Piece {
+    piece_type: PieceType,
+    color: Color
+}
+
+#[derive(Copy, Clone, Debug)]
 enum Color {
-    White,
-    Black
+    WHITE,
+    BLACK
 }
 
-enum Piece {
-    King(Color),
-    Queen(Color),
-    Knight(Color),
-    Rook(Color),
-    Bishop(Color),
-    Pawn(Color)
+#[derive(Copy, Clone, Debug, EnumIter)]
+enum PieceType {
+    ROOK,
+    KNIGHT,
+    BISHOP,
+    KING,
+    QUEEN,
+    PAWN,
+    EMPTY
 }
